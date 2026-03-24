62a274ff7164bc1f691b74f64a4468e1a2d425f1
S0umyajit <soumyajit2k18@gmail.com>
2026-03-24 13:00:18 +0530

    MultigenericsEx

---
+++ CollectionFramework/src/main/java/multiGenerics/MultiGenericEx.java
@@ -0,0 +1,11 @@
+package multiGenerics;
+
+public class MultiGenericEx {
+    public static void main(String[] args) {
+
+        Hashtable<Integer,String> hashtable=new Hashtable<>(12,"Soumya");
+
+        System.out.println(hashtable);
+    }
+
+}
