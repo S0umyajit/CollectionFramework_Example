7f45d3a8fda59f11ee74dc7ff1130951e12f5306
S0umyajit <soumyajit2k18@gmail.com>
2026-03-25 02:42:33 +0530

    BoundedTypeParamGenerics

---
+++ CollectionFramework/src/main/java/multiGenerics/Person.java
@@ -0,0 +1,32 @@
+package multiGenerics;
+
+public class Person implements Comparable<Person> {
+
+    private int age;
+    private String name;
+
+    Person(int age,String name){
+        this.age=age;
+        this.name=name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public int compareTo(Person otherPerson) {
+        return Integer.compare(this.age,otherPerson.getAge());
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
--- CollectionFramework/src/main/java/multiGenerics/TestBoundedEx.java
+++ CollectionFramework/src/main/java/multiGenerics/TestBoundedEx.java
@@ -3,5 +3,19 @@ package multiGenerics;
 public class TestBoundedEx {
     public static void main(String[] args) {
         System.out.println(BoundedTypeParamGenerics.min(122,23));
+
+        Person p1=new Person(12,"Soumya");
+        Person p2=new Person(23,"Anuj");
+
+        System.out.println(BoundedTypeParamGenerics.min(p1,p2));
+
+        int num=p1.compareTo(p2);
+
+        if (num<0){
+            System.out.println(p1.toString());
+        }
+        else {
+            System.out.println(p2.toString());
+        }
     }
 }
