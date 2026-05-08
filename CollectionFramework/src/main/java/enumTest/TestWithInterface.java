4228fc876fecf5474d14865331599cb126925053
S0umyajit <soumyajit2k18@gmail.com>
2026-05-09 00:49:19 +0530

    USING interface

---
+++ CollectionFramework/src/main/java/enumTest/TestWithInterface.java
@@ -0,0 +1,14 @@
+package enumTest;
+
+interface DaysOfWeeks{
+    String MONDAY="MONDAY";
+    String TUESDAY="TUESDAY";
+}
+
+public class TestWithInterface {
+    public static void main(String[] args) {
+
+        System.out.println(DaysOfWeeks.MONDAY);
+
+    }
+}
