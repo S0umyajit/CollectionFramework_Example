f2e29a5d8fd925940c59fcb90d645b6770d8f23d
S0umyajit <soumyajit2k18@gmail.com>
2026-05-09 00:46:59 +0530

    USING CONSTANTS

---
+++ CollectionFramework/src/main/java/enumTest/Prac.java
@@ -0,0 +1,14 @@
+package enumTest;
+class DaysOfWeek{
+    public static final String MONDAY="MONDAY";
+    public static final String TUESDAY="TUESDAY";
+    public static final String WEDNESDAY="WEDNESDAY";
+
+}
+public class Prac {
+    public static void main(String[] args) {
+
+        System.out.println(DaysOfWeek.MONDAY);
+
+    }
+}
