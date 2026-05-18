da34743093717c6a925a3ccd554488c5e3a85866
S0umyajit <soumyajit2k18@gmail.com>
2026-05-18 13:19:59 +0530

    stack using arrayList

--- CollectionFramework/src/main/java/stackPrac/StackTest.java
+++ CollectionFramework/src/main/java/stackPrac/StackTest.java
@@ -1,5 +1,6 @@
 package stackPrac;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Stack;
 
@@ -31,6 +32,16 @@ public class StackTest {
 
         Integer removedele=linkedList.removeLast();
         System.out.println("element removed from the last: "+removedele);
+
+        ArrayList<Integer>arrayList=new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+
+        System.out.println("arrayList: "+arrayList);
+
+        Integer removeElm=arrayList.remove(arrayList.size()-1);
+        System.out.println("remove from last ArrayList: "+removeElm);
 
     }
 }
