ea5039efe8cfbea66cf71216ea837ecde7eef52b
S0umyajit <soumyajit2k18@gmail.com>
2026-05-18 18:52:06 +0530

    hashMap ex

---
+++ CollectionFramework/src/main/java/mapPrac/TestMap.java
@@ -0,0 +1,32 @@
+package mapPrac;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class TestMap {
+    public static void main(String[] args) {
+        Map<Integer,String> map=new HashMap<>();
+
+        map.put(1,"Anish");
+        map.put(2,"Sayan");
+        map.put(3,"Anuj");
+
+        System.out.println(map);
+        Set<Integer> keys=map.keySet();
+        //entrySet to loop over the map
+        for(Integer key:keys){
+            System.out.println("loop using entryset: "+map.get(key));
+
+        }
+
+        //another way to loop over map- entrySet
+
+        Set<Map.Entry<Integer,String>> keyss=map.entrySet();
+        for(Map.Entry<Integer,String> key:keyss){
+            key.setValue(key.getValue().toUpperCase());
+            System.out.println(key.getKey()+" "+key.getValue());
+        }
+
+    }
+}
