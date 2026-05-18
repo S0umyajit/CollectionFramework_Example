94f1933e3c90e669a39e46c2f8f6e58cbdea7076
S0umyajit <soumyajit2k18@gmail.com>
2026-05-18 18:18:48 +0530

    copyOnWriteArrayList

--- CollectionFramework/src/main/java/copyOnArrayListTest/CopyOnWriteArrayListSolution.java
+++ CollectionFramework/src/main/java/copyOnArrayListTest/CopyOnWriteArrayListSolution.java
@@ -1,21 +1,63 @@
 package copyOnArrayListTest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CopyOnWriteArrayListSolution {
     public static void main(String[] args) {
-        CopyOnWriteArrayList<String> copyOnWriteArrayList=new CopyOnWriteArrayList<>();
-        copyOnWriteArrayList.add("bread");
-        copyOnWriteArrayList.add("jam");
-        copyOnWriteArrayList.add("tea");
+//        CopyOnWriteArrayList<String> copyOnWriteArrayList=new CopyOnWriteArrayList<>();
+//        copyOnWriteArrayList.add("bread");
+//        copyOnWriteArrayList.add("jam");
+//        copyOnWriteArrayList.add("tea");
+//
+//        for(String i:copyOnWriteArrayList){
+//            System.out.println("existing list: "+i);
+//
+//            if(i.equals("bread")){
+//                copyOnWriteArrayList.add("butter");
+//            }
+//        }
+//        System.out.println("new list: "+copyOnWriteArrayList);
 
-        for(String i:copyOnWriteArrayList){
-            System.out.println("existing list: "+i);
+//        List<String> newL=new ArrayList<>(); //if we use arraylist then it will throw error,
+        //As arraylist don't support it, modification while reading
+        List<String> newL=new CopyOnWriteArrayList<>();
+        newL.add("item1");
+        newL.add("item2");
+        newL.add("item3");
 
-            if(i.equals("bread")){
-                copyOnWriteArrayList.add("butter");
+        Thread readThread=new Thread(()->{
+            try{
+                while(true){
+            for(String e:newL) {
+                System.out.println("reading list: " + e);
+                Thread.sleep(500);
             }
-        }
-        System.out.println("new list: "+copyOnWriteArrayList);
+            }
+            }catch (InterruptedException e){
+                System.out.println(e.getMessage());
+            }
+        });
+
+
+        Thread writeThread=new Thread(()->{
+            try{
+               Thread.sleep(500);
+               newL.add("item4");
+                System.out.println("added item4");
+
+               Thread.sleep(500);
+               newL.remove("item1");
+                System.out.println("removed item1");
+            }catch (InterruptedException e){
+                System.out.println(e.getMessage());
+            }
+        });
+
+        readThread.start();
+        writeThread.start();
+
+
     }
 }
