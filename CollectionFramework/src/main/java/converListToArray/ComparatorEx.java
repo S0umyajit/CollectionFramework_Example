51f1044f98f94cdbf751d2360bf13200895aac68
S0umyajit <soumyajit2k18@gmail.com>
2026-05-20 02:19:48 +0530

    comparator example

--- CollectionFramework/src/main/java/converListToArray/ComparatorEx.java
+++ CollectionFramework/src/main/java/converListToArray/ComparatorEx.java
@@ -6,30 +6,39 @@ public class ComparatorEx {
     public static void main(String[] args) {
 
         List<String> list= Arrays.asList("banana","papaya","mango");
+//
+//        //list.sort(null);
+//        System.out.println(list);
+//        Collections.sort(list);
+//        System.out.println(list);
 
-        //list.sort(null);
+        list.sort(new StringLengthComparator());
         System.out.println(list);
-        Collections.sort(list);
-        System.out.println(list);
 
 
-        List<Integer> myList=new ArrayList<>();
-
-        myList.add(2);
-        myList.add(1);
-        myList.add(3);
-
-        myList.sort(new MyComparator());
-        System.out.println(myList);
+//        List<Integer> myList=new ArrayList<>();
+//
+//        myList.add(2);
+//        myList.add(1);
+//        myList.add(3);
+//
+//        myList.sort(new MyComparator());
+//        System.out.println(myList);
 
     }
-
-
 }
 class MyComparator implements Comparator<Integer>{
 
     @Override
     public int compare(Integer o1, Integer o2) {
         return o2-o1;
+    }
+}
+
+class StringLengthComparator implements Comparator<String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o2.length()-o1.length();
     }
 }
