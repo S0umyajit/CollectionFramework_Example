2528e8171be60573a6db0a04607d427387beba91
S0umyajit <soumyajit2k18@gmail.com>
2026-05-20 02:50:57 +0530

    comparator example using lamda

--- CollectionFramework/src/main/java/compratorEx/Test.java
+++ CollectionFramework/src/main/java/compratorEx/Test.java
@@ -2,6 +2,7 @@ package compratorEx;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Test {
@@ -9,27 +10,39 @@ public class Test {
     public static void main(String[] args) {
         Student s1=new Student("Soumya",3.4);
         Student s2=new Student("Ananya",3.6);
+        Student s5=new Student("Sanjay",3.6);
         Student s3=new Student("Anuj",3.1);
         Student s4=new Student("Arunima",3.6);
 
         List<Student> list=new ArrayList<>();
 
-        List<Student> myList=Arrays.asList(s1,s2,s3,s4);
+        List<Student> myList=Arrays.asList(s1,s2,s3,s4,s5);
         //this will not work as it is casted to int so only int value will be taken here the int value for
         //all are 3
       //  myList.sort((a,b)-> (int) (a.getNumber()-b.getNumber()));
-        myList.sort((a,b)->{
-            if(a.getNumber()-b.getNumber()>0){
-                return 1;
-            }
-            else if (a.getNumber()-b.getNumber()<0){
-                return -1;
-            }
-            else {
-                return a.getName().compareTo(b.getName());
-            }
-        });
+
+        Comparator<Student> com=Comparator
+                .comparing(Student::getNumber)
+                .reversed()
+                .thenComparing(Comparator.comparing((Student s)->s.getName().length()).reversed())
+                .thenComparing(Student::getName);
+        myList.sort(com);
         System.out.println(myList);
+
+
+
+//        myList.sort((a,b)->{
+//            if(a.getNumber()-b.getNumber()>0){
+//                return 1;
+//            }
+//            else if (a.getNumber()-b.getNumber()<0){
+//                return -1;
+//            }
+//            else {
+//                return a.getName().compareTo(b.getName());
+//            }
+//        });
+//        System.out.println(myList);
     }
 }
 
