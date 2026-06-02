377c3343fa70176c963d8917b09cb96591076591
S0umyajit <soumyajit2k18@gmail.com>
2026-06-03 02:29:32 +0530

    vector thread safe

---
+++ CollectionFramework/src/main/java/myTest/VectorEx.java
@@ -0,0 +1,37 @@
+package myTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Vector;
+
+public class VectorEx {
+    public static void main(String[] args) {
+
+        Vector<Integer>a1=new Vector<>();
+
+        Thread t1=new Thread(()->{
+           for(int i=1;i<=1000;i++){
+               a1.add(i);
+           }
+        });
+
+        Thread t2=new Thread(()->{
+            for(int i=1;i<=1000;i++) {
+                a1.add(i);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+
+        try{
+            t1.join();
+            t2.join();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println(a1.size());
+    }
+}
